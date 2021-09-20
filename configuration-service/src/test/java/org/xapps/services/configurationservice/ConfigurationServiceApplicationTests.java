4cd22b7528149e57d5d9a777e6a947bf8358e65f
Camilo del Real <cjdelreal2009@gmail.com>
2021-09-20 16:47:15 +0200

    ADD: Configuration service

---
+++ configuration-service/src/test/java/org/xapps/services/configurationservice/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xapps.services.configurationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
