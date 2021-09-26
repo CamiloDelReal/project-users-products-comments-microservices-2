950ca2ccba719bf6d6a35205e092e8f0f3cdef37
Camilo del Real <cjdelreal2009@gmail.com>
2021-09-26 11:47:48 +0200

    ADD: Packaging script

---
+++ scripts/package-services.bat
@@ -0,0 +1,63 @@
+@echo off
+
+echo [!] Checking maven
+set "mvnErr=0"
+call mvn -v || set mvnErr=1
+if "%mvnErr%" == "0" echo [+] Maven found && goto execute
+
+echo [-] Maven executable (mvn) could not be found
+echo [!] Please add mvn executable to your PATH variable
+echo [-] Script cannot continue
+goto end
+
+:execute
+
+@rem Configuration Service
+echo [!] Packaging Configuration Service
+cd ../configuration-service/
+set "mvnErr=0"
+call mvn2 clean package install "-Dmaven.test.skip=true" || set mvnErr=1
+if "%mvnErr%" == "0" echo [+] Configuration Service successfully packaged
+if "%mvnErr%" == "1" echo [-] Error packaging Configuration Service. Script cannot continue && goto end
+
+@rem Discovery Service
+echo [!] Packaging Discovery Service
+cd ../discovery-service/
+set "mvnErr=0"
+call mvn clean package install "-Dmaven.test.skip=true" || set mvnErr=1
+if "%mvnErr%" == "0" echo [+] Discovery Service successfully packaged
+if "%mvnErr%" == "1" echo [-] Error packaging Discovery Service. Script cannot continue && goto end
+
+@rem Gateway Service
+echo [!] Packaging Gateway Service
+cd ../gateway-service/
+set "mvnErr=0"
+call mvn clean package install "-Dmaven.test.skip=true" || set mvnErr=1
+if "%mvnErr%" == "0" echo [+] Gateway Service successfully packaged
+if "%mvnErr%" == "1" echo [-] Error packaging Gateway Service. Script cannot continue && goto end
+
+@rem Users Service
+echo [!] Packaging Users Service
+cd ../users-service/
+set "mvnErr=0"
+call mvn clean package install "-Dmaven.test.skip=true" || set mvnErr=1
+if "%mvnErr%" == "0" echo [+] Users Service successfully packaged
+if "%mvnErr%" == "1" echo [-] Error packaging Users Service. Script cannot continue && goto end
+
+@rem Products Service
+echo [!] Packaging Products Service
+cd ../products-service/
+set "mvnErr=0"
+call mvn clean package install "-Dmaven.test.skip=true" || set mvnErr=1
+if "%mvnErr%" == "0" echo [+] Products Service successfully packaged
+if "%mvnErr%" == "1" echo [-] Error packaging Products Service. Script cannot continue && goto end
+
+@rem Comments Service
+echo [!] Packaging Comments Service
+cd ../comments-service/
+set "mvnErr=0"
+call mvn clean package install "-Dmaven.test.skip=true" || set mvnErr=1
+if "%mvnErr%" == "0" echo [+] Comments Service successfully packaged
+if "%mvnErr%" == "1" echo [-] Error packaging Comments Service. Script cannot continue && goto end
+
+:end
